286ab32f1ced7d63b6606f4f2db64d58746f0da1
herbert.ho <herbertho@commercialpress.com.hk>
2016-07-12 11:52:04 +0800

    photo resolution update and config!

---
+++ application/config/photo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HerbertHo
+ * Date: 7/12/2016
+ * Time: 11:27 AM
+ */
+$config["photo_long_side"] = array(480, 800, 1280, 1680, 1920);
--- application/controllers/admin/Album_control.php
+++ application/controllers/admin/Album_control.php
@@ -15,6 +15,7 @@ class Album_control extends CI_Controller
 		$this->load->library("JSONAPI");
 		$this->load->library("JSONAPIEnum");
 		$this->load->library("form_validation");
+		$this->load->config("photo");
 
 		$this->config_validation_add_album =  array(
 			array(
@@ -456,9 +457,10 @@ class Album_control extends CI_Controller
 			{
 				if (move_uploaded_file($tempFile, $target_file))
 				{
-					$this->resize_photo(1680, $target_file);
-					$this->resize_photo(1280, $target_file);
-					$this->resize_photo(800, $target_file);
+					foreach($this->config->item("photo_long_side") as $value)
+					{
+						$this->resize_photo($value, $target_file);
+					}
 
 					JSONAPI::echo_json_successful_response();
 				}
@@ -500,6 +502,11 @@ class Album_control extends CI_Controller
 		$config['create_thumb'] = TRUE;
 		$config['maintain_ratio'] = TRUE;
 		$config['quality'] = '100%';
+
+		if ($width < $this->config->item("photo_long_side")[0] &&  $height < $this->config->item("photo_long_side")[0])
+		{
+			return;
+		}
 
 		if ($width >= $long_side || $height >= $long_side)
 		{
