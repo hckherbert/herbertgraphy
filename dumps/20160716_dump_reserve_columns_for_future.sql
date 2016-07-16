2420621b420127aa9e10b7f0b2eb356cf99f0a33
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-07-16 13:31:01 +0800

    change table structure to reserve future functions!

---
+++ dumps/20160716_dump_reserve_columns_for_future.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 16, 2016 at 01:27 PM
+-- Server version: 5.5.37
+-- PHP Version: 5.4.28
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `herbertgra_main`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `parentId` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `intro` text COLLATE utf8_unicode_ci,
+  `visible` tinyint(1) DEFAULT '1',
+  `view_count` int(11) DEFAULT NULL,
+  `privatekey` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE IF NOT EXISTS `photos` (
+  `photoId` int(11) NOT NULL AUTO_INCREMENT,
+  `albumId` int(11) NOT NULL,
+  `album_code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `slug_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `hash_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `desc` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `featured` tinyint(1) DEFAULT '0',
+  `visible` tinyint(1) DEFAULT '1',
+  `highlighted` tinyint(1) DEFAULT '0',
+  `view_count` int(11) DEFAULT NULL,
+  `exif` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`photoId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci4 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
