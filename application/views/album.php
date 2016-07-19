d4bf06c87d9d411fa6d5eac7246dffc9551de1e0
herbert.ho <herbertho@commercialpress.com.hk>
2016-07-19 15:55:03 +0800

    remove html xmlns

--- application/views/album.php
+++ application/views/album.php
@@ -1,7 +1,6 @@
-<?php 
-?>
+<?php ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
