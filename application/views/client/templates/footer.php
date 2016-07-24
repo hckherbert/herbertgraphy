e3156c76777840356fbe48210167d5e379984f39
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-07-25 00:35:51 +0800

    footer menu icons!

--- application/views/client/templates/footer.php
+++ application/views/client/templates/footer.php
@@ -1,9 +1,12 @@
 <?php ?>
 <div class="footer">
-    <div class="footerMenu">
-        <a id="btn_ask_her" href="javascript:void(0)"></a>
-        <a id="btn_update_her" href="javascript:void(0)"></a>
-        <a id="btn_share_her" href="javascript:void(0)"></a>
+    <div class="footerMenuWrapper">
+        <div class="footerMenu">
+            <a id="btn_ask_her" href="javascript:void(0)"></a>
+            <a id="btn_update_her" href="javascript:void(0)"></a>
+            <a id="btn_share_her" href="javascript:void(0)"></a>
+        </div>
     </div>
+    <div class="bg"></div>
     <div class="text">&copy; <?php echo date("Y"); ?> HerbertGraphy</div>
 </div>
