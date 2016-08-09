af29878cd203fa3a50391acdcc6a7b629ecbc5a3
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-09 22:20:07 +0800

    title font size use vew, footer update

--- application/views/client/templates/footer.php
+++ application/views/client/templates/footer.php
@@ -1,11 +1,9 @@
 <?php ?>
 <div class="footer<?php if (isset($is_base) && $is_base === true){ ?>  show<?php }?>">
     <div class="footerMenuWrapper">
-        <div class="footerMenu">
-            <a id="btn_ask_her" href="javascript:void(0)"></a>
-            <a id="btn_update_her" href="javascript:void(0)"></a>
-            <a id="btn_share_her" href="javascript:void(0)"></a>
-        </div>
+        <a id="btn_ask_her" href="javascript:void(0)"></a>
+        <a id="btn_update_her" href="javascript:void(0)"></a>
+        <a id="btn_share_her" href="javascript:void(0)"></a>
     </div>
     <div class="bg"></div>
     <div class="text">&copy; <?php echo date("Y"); ?> Herbertgraphy</div>
