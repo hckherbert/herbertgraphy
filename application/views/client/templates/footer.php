4e33bfcf1f85ad494e8a2a70e590b36f02dd25b1
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-17 23:19:24 +0800

    css update ,about btn

--- application/views/client/templates/footer.php
+++ application/views/client/templates/footer.php
@@ -1,9 +1,7 @@
 <?php ?>
 <div class="footer<?php if (isset($is_base) && $is_base == true){ ?>  show<?php }?>">
     <div class="footerMenuWrapper">
-        <a id="btn_ask_her" href="javascript:void(0)"></a>
-        <a id="btn_update_her" href="javascript:void(0)"></a>
-        <a id="btn_share_her" href="javascript:void(0)"></a>
+        <a id="btn_about_her" href="javascript:void(0)" title="All about her"></a>
     </div>
     <div class="bg"></div>
     <div class="text">&copy; <?php echo date("Y"); ?> HerbertGraphy</div>
