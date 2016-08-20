f7e4d8c76b192139a440048f6f167853053c671f
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-20 16:41:33 +0800

    update about words

--- application/views/client/about/main.php
+++ application/views/client/about/main.php
@@ -1,7 +1,7 @@
 <div class="about">
-    <p>In 2008, I got my first SLR camera. Since then I developed an interest in photography. Here is a showcase of the photographs that I’ve been taking. Everything here is organized by me, Herbert - from every photograph, layout design and  technical stuff like programming etc. of the site. I see it a way to present my passion for what I’ve been doing  - passion, sincerity and uniqueness. This is what HerbertGraphy wants to convey.</p>
+    <p>In 2008, I got my first SLR camera. Since then I developed an interest in photography. Here is a showcase of the photographs that I’ve been taking. Everything here is organized by me, Herbert - from every photograph, layout design and  technical stuff like programming etc. of the site. I see it a way to present my passion for what I’ve been doing  - passion, sincerity and uniqueness.</p>
     <br>
-    <p>I read books particularly in art, philosophy, history and photography to get inspirations. I believe reading is one of the best resources to keep my mind fresh, when people are told to pursue new technologies. It doesn’t mean they both conflict with each other; it’s just choosing the right thing to do at the right time.</p>
+    <p>I get inspirations from books. I believe reading is one of the best resources to keep my mind fresh. To me every album is like a book, in which every photo is a chapter, and it must have a message to convey, or even a story itself.</p>
     <br>
     <p>All of the photographs here, except those in the travel category,  are taken in Hong Kong - where I was born and where I will keep staying in. I particularly  like the dusk in Hong Kong, and you can find how captivating it is here, especially when you <em>accidentally</em> reach HerbertGraphy by googling from somewhere else. My camera has also followed me to Japan, Turkey, Nepal, Poland, France, Italy and Australia.</p>
     <br>
