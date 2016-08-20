a8972bad20453e8be2a216c3c8e0701033aa52b6
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-21 00:24:34 +0800

    home word experiemnt

--- application/views/client/home/main.php
+++ application/views/client/home/main.php
@@ -1,2 +1,3 @@
 <img id="cover" src="<?php echo base_url("assets/test/sopot.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
-<div class="homeSubTitle">Herbert Ho's photography showcase</div>
+<div class="homeSubTitle_c">I STRETCH the solo photograph to <br>make it the <em>dominant</em> cover.</div>
+<div class="homeSubTitle_d">I place the chapters in GRIDS to produce <em>abundance</em> and <em>coherence</em>.</div>
