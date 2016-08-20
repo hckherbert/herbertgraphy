0ea3e3b79e6ec2750be2527de5612a6dc2dfcb70
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-21 00:37:22 +0800

    cover exp

--- application/views/client/home/main.php
+++ application/views/client/home/main.php
@@ -1,3 +1,4 @@
 <img id="cover" src="<?php echo base_url("assets/test/sopot.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
 <div class="homeSubTitle_c">I STRETCH the solo photograph to <br>make it the <em>dominant</em> cover.</div>
 <div class="homeSubTitle_d">I place the chapters in GRIDS to produce <em>abundance</em> and <em>coherence</em>.</div>
+<div class="homeSubTitle_e">HerbertGraphy / Herbert Ho's photography showcase / Cover: Sopot Molo</div>
