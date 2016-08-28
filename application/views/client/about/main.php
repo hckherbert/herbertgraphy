704c065f14fe0bf4f7566be60d48ba84d672ccd7
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-28 19:23:35 +0800

    update about words

--- application/views/client/about/main.php
+++ application/views/client/about/main.php
@@ -1,12 +1,10 @@
 <div class="aboutContainer bgBase">
     <article>
-        <p>In 2008, I got my first SLR camera. Since then I developed an interest in photography. Here is a showcase of the photographs that I’ve been taking. Everything here is organized by me, Herbert - from every photograph, layout design and  technical stuff like programming etc. of the site. I see it a way to present my passion for what I’ve been doing  - passion, sincerity and uniqueness.</p>
+        <p>I am Herbert, a photography lover, and here is a showcase of the photographs I have been taking in these years. Like poems, chapters are re-arranged by editors so that the words are dancing in with the right rhythms, my photo albums are recompiled so they would render in the best form. Everything here is organized by me to show my passion, sincerity and uniqueness.</p>
         <br>
-        <p>I get inspirations from books. I believe reading is one of the best resources to keep my mind fresh. To me every album is like a book, in which every photo is a chapter, and it must have a message to convey, or even a story itself.</p>
+        <p>All of the photographs here, except those in the travel category,  are taken in Hong Kong. I particularly like the <em>major hours</em> in Hong Kong, and you can find how captivating it is here, especially when you accidentally googled HerbertGraphy from somewhere else. My camera has also followed me to Japan, Turkey, Nepal, Poland, France, Italy and Australia.</p>
         <br>
-        <p>All of the photographs here, except those in the travel category,  are taken in Hong Kong - where I was born and where I will keep staying in. I particularly  like the dusk in Hong Kong, and you can find how captivating it is here, especially when you <em>accidentally</em> reach HerbertGraphy by googling from somewhere else. My camera has also followed me to Japan, Turkey, Nepal, Poland, France, Italy and Australia.</p>
-        <br>
-        <p>I’m glad to have received very positive feedbacks from my friends on the photographs. I’ll keep HerbretGraphy up-to-date, and bring more surprises from time to time. I enjoy most when I’m asked to take portrait photos. If you wanna become one of the lucky ones who appear here, just ask <em>her</em> out:</p>
+        <p>I am glad to have received very positive impressions on the photographs from my friends. . I’ll keep HerbretGraphy up-to-date, and bring more surprises from time to time. I enjoy most when I’m asked to take portrait photos. If you wanna become one of the lucky ones to appear here, try to reach me at:</p>
         <br>
         <br>
         <p><span class="simple_icon">&#9993;</span>info@herbertgraphy.com</p>
