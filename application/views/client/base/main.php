1a6ecff8966a205fcbefcfa12c59e909a65e4c8e
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-28 22:02:31 +0800

    not found

--- application/views/client/base/main.php
+++ application/views/client/base/main.php
@@ -1,1 +1,1 @@
-<img id="cover" src="<?php echo base_url("assets/test/sopot2.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
+<img id="cover" src="<?php echo base_url("assets/images/not_found.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
