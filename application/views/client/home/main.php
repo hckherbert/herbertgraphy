5bc5bfe3130507cea81cf8d57b6d7549c2cb55c7
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-28 20:32:49 +0800

    update cover, update intro words

--- application/views/client/home/main.php
+++ application/views/client/home/main.php
@@ -1,4 +1,4 @@
-<img id="cover" src="<?php echo base_url("assets/test/sopot.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
-<div class="homeSubTitle_c"><span class="first_line">I <span class="cap">RECOMPILE</span> my photographs here to </span><span class="second_line"> turn them into a <span class="cap">CONSPICUOUS</span> form</span></div>
+<img id="cover" src="<?php echo base_url("assets/images/cover.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
+<div class="homeSubTitle_c"><span class="first_line">I <span class="cap">RECOMPILE</span> my photographs here</span><span class="second_line"> as if words <span class="cap">FLUTTER</span> and <span class="cap">SPIN</span></span></div>
 <div class="homeSubTitle_d"><em>Herbert Ho's Photography showcase</em></div>
 <div class="homeSubTitle_e">HerbertGraphy | Herbert Ho's photography showcase | Cover: Sopot Molo</div>
