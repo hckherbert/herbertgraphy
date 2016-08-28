b282575c9f5b2ee483502df11adf4d4fbaf42723
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-08-28 19:24:53 +0800

    update about words

--- application/views/client/about/main.php
+++ application/views/client/about/main.php
@@ -2,7 +2,7 @@
     <article>
         <p>I am Herbert, a photography lover, and here is a showcase of the photographs I have been taking in these years. Like poems, chapters are re-arranged by editors so that the words are dancing in with the right rhythms, my photo albums are recompiled so they would render in the best form. Everything here is organized by me to show my passion, sincerity and uniqueness.</p>
         <br>
-        <p>All of the photographs here, except those in the travel category,  are taken in Hong Kong. I particularly like the <em>major hours</em> in Hong Kong, and you can find how captivating it is here, especially when you accidentally googled HerbertGraphy from somewhere else. My camera has also followed me to Japan, Turkey, Nepal, Poland, France, Italy and Australia.</p>
+        <p>All of the photographs here, except those in the travel category,  are taken in Hong Kong. I particularly like the <em>magic hours</em> in Hong Kong, and you can find how captivating it is here, especially when you accidentally googled HerbertGraphy from somewhere else. My camera has also followed me to Japan, Turkey, Nepal, Poland, France, Italy and Australia.</p>
         <br>
         <p>I am glad to have received very positive impressions on the photographs from my friends. . I’ll keep HerbretGraphy up-to-date, and bring more surprises from time to time. I enjoy most when I’m asked to take portrait photos. If you wanna become one of the lucky ones to appear here, try to reach me at:</p>
         <br>
