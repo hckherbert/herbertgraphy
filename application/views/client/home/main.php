012426e8186e0b5817603d6b638312f5da2bfdcb
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-09-13 17:37:10 +0800

    update home message!

--- application/views/client/home/main.php
+++ application/views/client/home/main.php
@@ -1,4 +1,4 @@
 <img id="cover" src="<?php echo base_url("assets/images/cover.jpg"); ?>" style="position:absolute;top:0;left:0;width:100%;">
-<div class="homeSubTitle_c"><span class="first_line">I <span class="cap">RECOMPILE</span> my photographs here</span><span class="second_line"> as if words <span class="cap">FLUTTER</span> and <span class="cap">SPIN</span></span></div>
+<div class="homeSubTitle_c">When there is too much noise</div>
 <div class="homeSubTitle_d"><em>Herbert Ho's Photography showcase</em></div>
 <div class="homeSubTitle_e">HerbertGraphy | Herbert Ho's photography showcase | Cover: Sopot Molo</div>
