9da41e7e85003931436b51f758469127039b598e
herbert.ho <herbertho@commercialpress.com.hk>
2016-09-28 11:26:46 +0800

    add og:image width height
    todo: get real width height from backend

--- application/views/client/album/meta_tags.php
+++ application/views/client/album/meta_tags.php
@@ -3,3 +3,5 @@
 <?php } else if (isset($featured_photo) && $featured_photo!=NULL) { ?>
     <meta property="og:image" content="<?php echo $featured_photo; ?>" />
 <?php } ?>
+<meta property="og:image:width" content="1280" />
+<meta property="og:image:height" content="850" />
--- application/views/client/templates/common_include.php
+++ application/views/client/templates/common_include.php
@@ -16,14 +16,14 @@
     <meta name="format-detection" content="telephone=no" />
     <meta name="description" content="HerbertGraphy - Herbert Ho's photography showcase">
     <meta name="keywords" content="HerbertGraphy<?php  if (isset($title) && $title !=NULL) { echo " - ".$title;} ?>">
-    <meta property="og:url" content="<?php echo site_url(); ?>" />
+    <!--<meta property="og:url" content="<?php echo site_url(); ?>" />-->
     <meta property="og:description" content="HerbertGraphy - Herbert Ho's photography showcase" />
     <meta property="og:title" content="HerbertGraphy<?php if (isset($title) && $title !=NULL) { echo " - ".$title;} ?>" />
-    <meta name="twitter:description" content="HerbertGraphy - Herbert Ho's photography showcase" />
-    <meta name="twitter:title" content="HerbertGraphy<?php if (isset($title) && $title !=NULL) { echo " - ".$title;} ?>" />
     <?php if (isset($meta_tags)){
         $this->load->view($meta_tags);
     } ?>
+    <meta name="twitter:description" content="HerbertGraphy - Herbert Ho's photography showcase" />
+    <meta name="twitter:title" content="HerbertGraphy<?php if (isset($title) && $title !=NULL) { echo " - ".$title;} ?>" />
     <title>
         <?php if (isset($home_title) && $home_title !=NULL) { ?>
         HerbertGraphy<?php echo " - ".$home_title;?>
