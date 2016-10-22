77cf8a3d2ff86f97f1b2af0193cbc54e23b616a8
herbert.ho <HerbertHo@commercialpress.com.hk>
2016-10-22 22:35:30 +0800

    update about words

--- application/views/client/about/main.php
+++ application/views/client/about/main.php
@@ -1,10 +1,10 @@
 <div class="aboutContainer bgBase">
     <article>
-        <p>I am Herbert, a photography lover from HK, and here is a showcase of the photographs I have been taking in these years. The photo albums are recompiled so they would be presented in the best form. Everything here is organized by me - from every photograph to the website constructions. I see it a way to show my passion, sincerity and uniqueness.</p>
+        <p>Isn't this guy cool? Yes it's me, Herbert, the manager of HerbertGraphy. Here is a showcase of the photographs I have been taking in these years. The photo albums are recompiled so they would be presented in the best form. Everything here is organized by Herbert - from every photograph to the website constructions. I see it a way to show my passion, sincerity and uniqueness.</p>
         <br>
-        <p>All of the photographs here, except those in the travel category, are taken in Hong Kong. I particularly like the <em>magic hours</em> in Hong Kong, and you can find how captivating it is here, especially when you have accidentally googled HerbertGraphy from somewhere else.</p>
+        <p>All of the photographs here, except those in the travel category, are taken in Hong Kong. I particularly like the <em>magic hours</em> in Hong Kong, and you can find how captivating it is here, especially when you reach accidentally by googling <em>HerbertGraphy</em> from outside Hong Kong.</p>
         <br>
-        <p>I am glad to have received very positive impressions on the photographs from my friends. . I’ll keep HerbretGraphy up-to-date, and bring more surprises from time to time. I enjoy most when taking portrait photos. If you wanna become one of the lucky ones to appear here, or if you come up with great ideas, just reach me at:</p>
+        <p>I am glad to have received very positive impressions on the photographs from my friends. I’ll keep HerbretGraphy up-to-date, and bring more surprises from time to time. I enjoy most when taking portrait photos. I would like to give special thanks to my friend Hammy, who helped capture this cool profile photo of me. If you wanna become one of the lucky ones to appear here, or if you come up with great ideas, just reach me at:</p>
         <div class="contacts">
             <p class="name">Herbert Ho</p>
             <span class="simple_icon">&#9993;</span>
@@ -15,3 +15,4 @@
         <div class="clear"></div>
     </article>
 </div>
+
