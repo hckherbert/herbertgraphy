96927d3f45bf6ce6a00e0cd818890c69b4110e3e
herbert.ho <herbertho@commercialpress.com.hk>
2017-03-14 12:22:42 +0800

    update about

--- application/views/client/about/main.php
+++ application/views/client/about/main.php
@@ -1,6 +1,6 @@
 <div class="aboutContainer bgBase">
     <article>
-        <p>Hello everyone, I'm Herbert from Hong Kong, a young guy who's fond of photography. Here is a showcase of the photographs I have been taking in these years. The photo albums are rearranged as if they are chapters reviewed by editors. Everything here is organized by Herbert - from every photograph to the website constructions. I see it a way to present my passion, sincerity and uniqueness.</p>
+        <p>Hello everyone, I'm Herbert from Hong Kong, a post-80's guy who's fond of photography. Here is a showcase of the photographs I have been taking in these years. The photo albums are rearranged as if they are chapters reviewed by editors. Everything here is organized by Herbert - from every photo shot to the website presentation. I find it a way to express my passion, sincerity and uniqueness.</p>
         <br>
         <p>All of the photographs here, except those in the <a href="../album/travel/">travel</a> category, are taken in Hong Kong. I particularly like the <em>magic hours</em> in Hong Kong, and you can find how captivating it is here, especially when you reach here accidentally by googling maybe <em>Herbert photography</em> from outside Hong Kong.</p>
         <br>
