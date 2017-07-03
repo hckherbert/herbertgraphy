22f2ed5eaf3e36183eab373178cbfe35d0d436ed
herbert.ho <herbertho@commercialpress.com.hk>
2017-07-03 15:11:23 +0800

    add on complete slide

--- assets/js/featured.js
+++ assets/js/featured.js
@@ -7,6 +7,7 @@ var mTweenDurationSliding = 0.7;
 var mTweenDurationImgOpacity = 0.2;
 var mWinWidthMidPoint = 0;
 var mTotalSlide = 0;
+var mIsSliding = false;
 
 $(document).ready(
     function()
@@ -27,6 +28,13 @@ $(document).ready(
             {
                 pEvent.preventDefault();
 
+                if (mTotalSlide == mActiveIndex+1 || mIsSliding)
+                {
+                    return;
+                }
+
+                mIsSliding = true;
+
                 var _timeLineCarousel = new TimelineLite();
                 var _timeLineImageOpacity = new TimelineLite();
 
@@ -39,14 +47,54 @@ $(document).ready(
                 //The shift half the targetItem width
                 var _targetLeftPos = _nextItemToLeft - _nextItem.width()* 0.5;
 
-
-                _timeLineCarousel.to($(".featuredList"),mTweenDurationSliding,{css:{left:  _targetLeftPos + "px"},ease:Circ.easeOut});
+                _timeLineCarousel.to($(".featuredList"),mTweenDurationSliding,{css:{left:  _targetLeftPos + "px"},ease:Circ.easeOut, onComplete: onSlideComplete});
                 _timeLineImageOpacity.to(_currentItem, mTweenDurationImgOpacity, {css:{opacity:0.3},ease:Circ.easeOut})
                          .to(_nextItem, mTweenDurationImgOpacity, {css:{opacity:1},ease:Circ.easeIn});
 
                 mActiveIndex++;
 
                 if (mTotalSlide == mActiveIndex+1)
+                {
+                    $(this).addClass("disable");
+                }
+                else
+                {
+                    $(this).removeClass("disable");
+                }
+
+            });
+
+
+            $(".navigator .btn_prev").on("click", function(pEvent)
+            {
+                pEvent.preventDefault();
+
+                if (mActiveIndex == 0 || mIsSliding)
+                {
+                    return;
+                }
+
+                mIsSliding = true;
+
+                var _timeLineCarousel = new TimelineLite();
+                var _timeLineImageOpacity = new TimelineLite();
+
+                var _currentItem = $(".featuredList img:eq(" + mActiveIndex + ")");
+                var _nextItem = $(".featuredList img:eq(" + (mActiveIndex - 1) + ")");
+                var _currentLeft = $(".featuredList").position().left;
+
+                //shift half the currentItem width first
+                var _prevItemToLeft = _currentLeft  + _currentItem.width()* 0.5;
+                //The shift half the targetItem width
+                var _targetLeftPos = _prevItemToLeft + _nextItem.width()* 0.5;
+
+                _timeLineCarousel.to($(".featuredList"),mTweenDurationSliding,{css:{left:  _targetLeftPos + "px"},ease:Circ.easeOut, onComplete: onSlideComplete});
+                _timeLineImageOpacity.to(_currentItem, mTweenDurationImgOpacity, {css:{opacity:0.3},ease:Circ.easeOut})
+                    .to(_nextItem, mTweenDurationImgOpacity, {css:{opacity:1},ease:Circ.easeIn});
+
+                mActiveIndex--;
+
+                if (mActiveIndex == 0)
                 {
                     $(this).addClass("disable");
                 }
@@ -92,4 +140,9 @@ function windowOnResized()
         _accumulatedWidth += _currentWidth;
 
     });
+}
+
+function onSlideComplete()
+{
+    mIsSliding = false;
 }
