2756842cc37aae2097a28500ab4cf2f12f5a8de5
herbert.ho <HerbertHo@commercialpress.com.hk>
2017-07-22 22:24:42 +0800

    temporarily remove footer featured icon

--- application/views/client/templates/footer.php
+++ application/views/client/templates/footer.php
@@ -3,7 +3,7 @@
     <div class="footerMenuWrapper">
         <a id="btn_home" class="hvr-pop"  href="<?php echo base_url(); ?>" title="Home"></a>
         <a id="btn_about_her" class="hvr-pop"  href="<?php echo base_url("bio"); ?>" title="Bio"></a>
-        <a id="btn_texts" class="hvr-pop"  href="<?php echo base_url("texts"); ?>" title="Texts"></a>
+        <!--<a id="btn_texts" class="hvr-pop"  href="<?php echo base_url("texts"); ?>" title="Texts"></a>-->
     </div>
     <div class="bg"></div>
     <div class="text">&copy; <?php echo date("Y"); ?> HerbertGraphy</div>
